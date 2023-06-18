216c806d3e878770980ded2e5e9a140840e15383
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-06-18 10:52:43 +0200

    Experiementing with components

--- MarkDownStorage/Program.cs
+++ MarkDownStorage/Program.cs
@@ -1,36 +1,29 @@
-using Blogging.Data;
-using Blogging.Models;
-using Microsoft.EntityFrameworkCore;
+using BlazorApp1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddMvc().AddControllersAsServices();
-
-builder.Services.AddDbContext<FileContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
