422aa568986d4ceb29132118e6298e047f5783b3
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-06-25 13:46:27 +0200

    DisplayLabel logic

--- MarkDownStorage/Data/WeatherForecast.cs
+++ MarkDownStorage/Data/WeatherForecast.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorApp1.Data;
 
 public class WeatherForecast
@@ -8,5 +11,6 @@ public class WeatherForecast
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    [DisplayName(displayName: "Some display name here")]
     public string? Summary { get; set; }
 }
